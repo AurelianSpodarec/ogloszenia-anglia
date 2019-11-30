316581269ba8582790662dc0cac9a8d2756e24ad
Aurelian <aurelians@libero.it>
2019-11-30 22:23:07 +0100

    Updates

--- src/json/carListing.js
+++ src/json/carListing.js
@@ -12,26 +12,22 @@
     {
         "userId": 1,
         "id": 5,
-        "postedBy": "individual",
-        "location": "UK, Manchester",
-        "price": 34000,
+        "createdAt": 1575048230910,
         "coverImage": "https://thumbs.img-sprzedajemy.pl/1000x901c/0a/2e/52/mitsubishi-3000gt-gto-jdm-3000gt-malopolskie-tarnow-488612276.jpg",
-        "name": "My fast GT car - 1year old",
+        "title": "My fast GT car - 1year old",
         "description": "The car has been taken care of and used only for my commute to work. It has new knob and its really awesome to drive with. No crashes all clean, come and take it.",
-        "justAdded": true,
-        "carDetails": [{   
-            "brand": "Rolls-Royce",
-            "model": "Silver Spur",
-            "year": 2004
-        }],
-        "properties": [{
-            "mileage": 232049,
-            "bodyStyle": "Truck",
-            "transmision": "Automatic",
-            "fuel": "Electric",
-            "driveTrain": "4WD",
-            "seats": 8
-        }],
+        "price": 34000,
+        "location": "UK, Manchester",
+        "postedBy": "individual",
+        "brand": "Rolls-Royce",
+        "model": "Silver Spur",
+        "year": 2004,
+        "mileage": 232049,
+        "bodyStyle": "Truck",
+        "transmision": "Automatic",
+        "fuel": "Electric",
+        "driveTrain": "4WD",
+        "seats": 8,
         "media": [
             {
                 "id": 1,
@@ -40,24 +36,6 @@
             {
                 "id": 2,
                 "img": "https://meganracing.com/media/catalog/product/cache/6e104eba86855cb02bc9374335d46b7c/m/r/mr-ls-m3gt-2.jpg"
-            }
-        ]
-    },
-    {
-        "userId": 14,
-        "id": 22,
-        "price": 89500,
-        "location": "UK, Nottingham",
-        "name": "Cheap SUV",
-        "brand": "SUV",
-        "model": "Rover",
-        "bodyStyle": "Truck",
-        "transmision": "Automatic",
-        "fuel": "Gas",
-        "media": [
-            {
-                "id": 1,
-                "img": "https://cdn.motor1.com/images/mgl/eLqQM/s1/2019-range-rover-sentinel.jpg"
             }
         ]
     }
