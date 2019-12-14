b394d8fafd29f117e90a4eac721289b1b739d203
Aurelian <aurelians@libero.it>
2019-12-14 20:11:48 +0100

    Created the functionality for auth modal, needs refactor

--- src/components/AuthDialog/index.js
+++ src/components/AuthDialog/index.js
@@ -25,92 +25,34 @@ import {
 } from '@material-ui/core';
 import clsx from 'clsx';
 
-function AuthRegisterView() {
-    const classes = useStyles();
-    return (
-        <div>
+// function AuthView
 
-        </div>
-    )
-}
-
-function AuthLoginView() {
+function AuthDialog(props) {
     const classes = useStyles();
+    const { onClose, selectedValue, open } = props;
+
+    const [currentView, setView] = useState();
     const [isPasswordVisible, setPasswordVisibility] = useState(false)
+
+    const openSocialView = () => {
+        setView('AuthSocialView')
+    }
+
+    const openLoginView = () => {
+        setView('AuthLoginView')
+    }
+
+    const openRegisterView = () => {
+        setView('AuthRegisterView')
+    }
 
     const handleClickShowPassword = () => {
         setPasswordVisibility(isPasswordVisible === false ? true : false);
     }
 
-    return (
-        <div>
+    // const setView = () => {
 
-            <FormControl className={clsx(classes.margin, classes.textField)}>
-                <div>
-                    <FontAwesomeIcon icon="lock" />
-                </div>
-                <div>
-                    <InputLabel htmlFor="auth-login-password">Password</InputLabel>
-                    <Input
-                        id="auth-login-password"
-                        type={isPasswordVisible ? 'text' : 'password'}
-                        endAdornment={
-                            <InputAdornment position="end">
-                                <IconButton
-                                    aria-label="toggle password visibility"
-                                    onClick={handleClickShowPassword}
-                                // onMouseDown={handleMouseDownPassword}
-                                >
-                                    {isPasswordVisible ? <FontAwesomeIcon icon="eye-slash" /> : <FontAwesomeIcon icon="eye" />}
-                                </IconButton>
-                            </InputAdornment>
-                        }
-                    />
-                </div>
-            </FormControl>
-        </div>
-    )
-}
-
-function AuthSocialView() {
-    const classes = useStyles();
-    return (
-        <div>
-            <div className={classes.socialButtons}>
-                <Button className={classes.buttonFacebook}>
-                    <FontAwesomeIcon className={classes.socialButtonIcon} icon={['fab', 'facebook-f']} />
-                    <Typography component="span">Continue with Facebook</Typography>
-                </Button>
-                <Button className={classes.buttonGoogle}>
-                    <FontAwesomeIcon className={classes.socialButtonIcon} icon={['fab', 'google']} />
-                    <Typography>Continue with Google</Typography>
-                </Button>
-            </div>
-
-            <div>
-                <Button>Sign Up</Button>
-                <Button>Log In</Button>
-            </div>
-
-            <Typography className={classes.terms}>By clicking on "Sign Up", you agree on Ogloszenia Anglia <a href="#">Terms & Conditions</a> and <a href="#">Privacy Policy</a></Typography>
-
-        </div>
-    )
-}
-
-function AuthDialog(props) {
-    const classes = useStyles();
-    const { onClose, selectedValue, open } = props;
-    const { currentView, setView } = useState();
-
-    // const views = {
-    //     AuthSocialView,
-    //     AuthLoginView,
-    //     AuthRegisterView
     // }
-    const setView = () => {
-        setView()
-    }
 
     const handleClose = () => {
         onClose(selectedValue);
@@ -119,6 +61,89 @@ function AuthDialog(props) {
     const handleListItemClick = value => {
         onClose(value);
     };
+
+
+    const AuthLoginView = () => {
+
+        return (
+            <div>
+
+                <FormControl className={clsx(classes.margin, classes.textField)}>
+                    <div>
+                        <FontAwesomeIcon icon="lock" />
+                    </div>
+                    <div>
+                        <InputLabel htmlFor="auth-login-password">Password</InputLabel>
+                        <Input
+                            id="auth-login-password"
+                            type={isPasswordVisible ? 'text' : 'password'}
+                            endAdornment={
+                                <InputAdornment position="end">
+                                    <IconButton
+                                        aria-label="toggle password visibility"
+                                        onClick={handleClickShowPassword}
+                                    // onMouseDown={handleMouseDownPassword}
+                                    >
+                                        {isPasswordVisible ? <FontAwesomeIcon icon="eye-slash" /> : <FontAwesomeIcon icon="eye" />}
+                                    </IconButton>
+                                </InputAdornment>
+                            }
+                        />
+                    </div>
+                </FormControl>
+            </div>
+        )
+    }
+
+    const AuthRegisterView = () => {
+        return (
+            <div>
+                j
+            </div>
+        )
+    }
+
+
+    const AuthSocialView = () => {
+        return (
+            <div>
+                <div className={classes.socialButtons}>
+                    <Button className={classes.buttonFacebook}>
+                        <FontAwesomeIcon className={classes.socialButtonIcon} icon={['fab', 'facebook-f']} />
+                        <Typography component="span">Continue with Facebook</Typography>
+                    </Button>
+                    <Button className={classes.buttonGoogle}>
+                        <FontAwesomeIcon className={classes.socialButtonIcon} icon={['fab', 'google']} />
+                        <Typography>Continue with Google</Typography>
+                    </Button>
+                </div>
+
+                <div>
+                    <Button onClick={openRegisterView}>Sign Up</Button>
+                    <Button onClick={openLoginView}>Log In</Button>
+                </div>
+
+                <Typography className={classes.terms}>By clicking on "Sign Up", you agree on Ogloszenia Anglia <a href="#">Terms & Conditions</a> and <a href="#">Privacy Policy</a></Typography>
+
+            </div>
+        )
+    }
+
+    const renderAuthSocialConnector = (currentView) => {
+        switch (currentView) {
+            case "AuthSocialView":
+                return AuthSocialView();
+                break;
+            case "AuthLoginView":
+                return AuthLoginView();
+                break;
+            case "AuthRegisterView":
+                return AuthRegisterView();
+                break;
+            default:
+                return AuthSocialView();
+        }
+    }
 
     return (
         <Dialog modal={true} className={classes.dialog} onClose={handleClose} maxWidth={false} aria-labelledby="simple-dialog-title" open={open}>
@@ -143,9 +168,9 @@ function AuthDialog(props) {
                     </div>
 
                     <div className={classes.options}>
-                        {
-                            AuthLoginView()
 
+                        {
+                            renderAuthSocialConnector(currentView)
                         }
                     </div>
                 </div>
