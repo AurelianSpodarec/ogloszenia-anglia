55c379d9b813c5d2daf4430fa9de9abc46522a52
Aurelian <aurelians@libero.it>
2020-01-17 23:59:13 +0000

    Cleaned up context car list

--- src/context/CarsListContext.js
+++ src/context/CarsListContext.js
@@ -9,25 +9,19 @@ const CarListContext = createContext();
 function CarListProvider({ children }) {
 
     const [models, setModels] = useState([])
-
-
-    // const [postedBy, setPostedBy] = useState();
     const [make, setMake] = useState();
 
-    // const [year, setYear] = useState([INITIAL_CAR_STATE.year[0], INITIAL_CAR_STATE.year[1]]);
-    // const [mileage, setMileage] = useState([INITIAL_CAR_STATE.mileage[0], INITIAL_CAR_STATE.mileage[1]]);
-    // const [seats, setSeats] = useState([INITIAL_CAR_STATE.seats[0], INITIAL_CAR_STATE.seats[1]]);
 
 
     const [car, setCar] = useState({
-        // postedBy,
-        make,
-        // model,
-        // bodyStyle,
-        // transmission,
-        // year,
-        // mileage,
-        // seats
+        postedBy: 'All',
+        make: 'All',
+        model: 'All',
+        bodyStyle: 'All',
+        transmission: 'All',
+        year: [INITIAL_CAR_STATE.year[0], INITIAL_CAR_STATE.year[1]],
+        mileage: [INITIAL_CAR_STATE.mileage[0], INITIAL_CAR_STATE.mileage[1]],
+        seats: [INITIAL_CAR_STATE.seats[0], INITIAL_CAR_STATE.seats[1]]
     })
 
     const [carList, setCarList] = useState([])
