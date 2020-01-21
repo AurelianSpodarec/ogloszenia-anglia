472c58821aa7ebd42c1a5e59c99bcced26ce5bde
Aurelian <aurelians@libero.it>
2020-01-21 19:27:11 +0000

    Updated url to dynamic url API

--- src/services/config.js
+++ src/services/config.js
@@ -1,4 +1,4 @@
-const apiUrl = process.env.NODE_ENV === 'production' ? 'https://example.com' : process.env.REACT_APP_API_URL;
+const apiUrl = process.env.NODE_ENV === 'production' ? process.NODE_ENV.API_URL : process.env.REACT_APP_API_URL;
 
 
 export default {
