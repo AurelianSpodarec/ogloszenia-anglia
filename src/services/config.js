bd7aadba4b006e7767ae9d01da2fcdf489204e3e
Aurelian <aurelians@libero.it>
2020-01-22 23:55:09 +0000

    Hardcoded url

--- src/services/config.js
+++ src/services/config.js
@@ -1,4 +1,4 @@
-const apiUrl = process.env.NODE_ENV === 'production' ? process.env.API_URL : process.env.REACT_APP_API_URL;
+const apiUrl = process.env.NODE_ENV === 'production' ? "https://ogloszenia-anglia.herokuapp.com/api/v1/" : process.env.REACT_APP_API_URL;
 
 export default {
     apiUrl,
