0963bc1f5770494d07cf4412d4bb30d60d1b6584
Aurelian <aurelians@libero.it>
2020-01-28 23:57:21 +0000

    Made the basics working for adding new car

--- src/components/Dropzone/index.js
+++ src/components/Dropzone/index.js
@@ -1,5 +1,25 @@
-const Dropzone = function () {
 
+const Dropzone = function () {
+    // const classes = useStyles();
+
+    // return (
+    //     <Box className={classes.dropzone}>
+    //         <Box className={classes.dropzoneInner}>
+    //             <Box className={classes.dropzoneText}>
+    //                 <Typography>Drop & Drop or browser</Typography>
+    //                 <Typography>Upload up to 10photos of what you're selling. Images must be in PNG or JPG format and under 5mb.</Typography>
+    //                 {/* <Typography>or</Typography>
+    //                         <Button>Browse Files</Button> */}
+    //             </Box>
+    //         </Box>
+
+    //         {/* <Box>
+    //                     Image
+    //                 </Box>
+    //                 <Box></Box> */}
+    //     </Box>
+    // )
 }
 
 export default Dropzone;
+
--- src/views/AddListing/index.js
+++ src/views/AddListing/index.js
@@ -1,132 +1,26 @@
 import React, { useState, useEffect } from 'react';
 
 import PropTypes from 'prop-types';
-import { makeStyles } from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
-import DialogTitle from '@material-ui/core/DialogTitle';
-import Dialog from '@material-ui/core/Dialog';
-import Typography from '@material-ui/core/Typography';
+import { Dialog, Button, DialogTitle } from '@material-ui/core';
 
 import {
-    Box, FormControl, TextField, InputLabel,
+    Box,
+    FormControl,
+    TextField,
+    InputLabel,
     DialogContent,
-    DialogActions, Input,
-    OutlinedInput, InputAdornment
+    DialogActions,
+    Input,
+    InputAdornment
 } from '@material-ui/core/'
-
-
 
 import { INITIAL_CAR_STATE } from '@utils/data/menu';
 import { createCar } from '@services/api/categories/car';
 import { useForm } from '@hooks';
-
 import { CustomItem, CustomTextarea } from '@components';
+
 import useStyles from './styles'
 
-
-const Location = function () {
-    return (
-        // Current location - access geo data or if location is in profile
-        // Zipcode/Select location
-        <Box></Box>
-        // 
-    )
-}
-
-const ListingChooseCar = function () {
-    return (
-        <Box>
-            <Typography>Car model</Typography>
-
-            <CustomItem
-                label="Year"
-                // onClick={onPostedBy}
-                search
-                data={INITIAL_CAR_STATE.year}
-            />
-            <CustomItem
-                label="Make"
-                // onClick={onPostedBy}
-                search
-                data={INITIAL_CAR_STATE.make}
-            />
-            {/* Get by id selected */}
-            {/* <CustomItem
-                label="Model"
-                // onClick={onPostedBy}
-                search
-                data={INITIAL_CAR_STATE.model}
-            /> */}
-
-            <Typography>Car details</Typography>
-
-
-            <CustomItem
-                label="Body Style"
-                // onClick={onSelectBodyStyle}
-                data={INITIAL_CAR_STATE.bodyStyle}
-                multiSelect
-            />
-            <CustomItem
-                label="Transmission"
-                // onClick={onSelectTransmission}
-                data={INITIAL_CAR_STATE.transmission}
-                multiSelect
-            />
-            <CustomItem
-                label="Fuel"
-                data={INITIAL_CAR_STATE.fuel}
-                multiSelect
-            />
-            <CustomItem
-                label="Drivetrain"
-                data={INITIAL_CAR_STATE.driveTrain}
-                multiSelect
-            />
-            {/* <Divider /> */}
-
-            Seats
-            Mileage
-        </Box>
-    )
-}
-
-const ChooseCategory = function () {
-    const classes = useStyles();
-
-    return (
-        <Box>
-            <Typography>Choose a category</Typography>
-            <Box>Cars</Box>
-            <Box>Housing</Box>
-            <Box>Jobs</Box>
-        </Box>
-    )
-}
-
-
-// TODO: Make dropzone global
-const Dropzone = function () {
-    const classes = useStyles();
-
-    return (
-        <Box className={classes.dropzone}>
-            <Box className={classes.dropzoneInner}>
-                <Box className={classes.dropzoneText}>
-                    <Typography>Drop & Drop or browser</Typography>
-                    <Typography>Upload up to 10photos of what you're selling. Images must be in PNG or JPG format and under 5mb.</Typography>
-                    {/* <Typography>or</Typography>
-                            <Button>Browse Files</Button> */}
-                </Box>
-            </Box>
-
-            {/* <Box>
-                        Image
-                    </Box>
-                    <Box></Box> */}
-        </Box>
-    )
-}
 
 const INITIAL_NEW_CAR_STATE = {
     "title": "",
@@ -145,6 +39,51 @@ const INITIAL_NEW_CAR_STATE = {
     "fuel": "",
     "driveStrain": "",
     "seats": "",
+}
+
+
+
+const INITIAL_CUSTOM_DATA = {
+    seats: [
+        {
+            slug: "1",
+            displayName: '1'
+        },
+        {
+            slug: "2",
+            displayName: '2'
+        },
+        {
+            slug: "3",
+            displayName: '3'
+        },
+        {
+            slug: "4",
+            displayName: '4'
+        },
+        {
+            slug: "5",
+            displayName: '5'
+        },
+        {
+            slug: "6",
+            displayName: '6'
+        },
+        {
+            slug: "7",
+            displayName: '7'
+        },
+        {
+            slug: "8",
+            displayName: '8'
+        },
+        {
+            slug: "9",
+            displayName: '9'
+        },
+
+    ]
+
 }
 
 const AddListing = function ({ onClose, selectedValue, open }) {
@@ -206,7 +145,6 @@ const AddListing = function ({ onClose, selectedValue, open }) {
 
             <DialogContent dividers='paper'>
 
-
                 <CustomItem
                     name="postedBy"
                     label="Posted By"
@@ -239,19 +177,55 @@ const AddListing = function ({ onClose, selectedValue, open }) {
                     data={INITIAL_CAR_STATE.fuel}
                     menuPosition="bottom"
                 />
-                {/*<CustomSlider
-                    // slideBeforeLabel
-                    leftBeforeSlideMaxLabel={"Before"}
-                    label="Year"
-                    valueMatchLabel="Any year" // If values match display 'Any year', instead of 1960-2020
-                    min={INITIAL_CAR_STATE.year[0]}
-                    max={INITIAL_CAR_STATE.year[1]}
+
+                {/* TODO: Need to create years array and re-factor this with a for loop from 1960 -2020*/}
+                <CustomItem
                     name="year"
-                    value={year}
-                    // onChange={handleChange}
-                    onChange={onYearChange}
+                    label="Year"
+                    onClick={(e, value, name) => onSelected(e, value, name)}
+                    data={[
+                        {
+                            slug: "2020",
+                            displayName: '2020'
+                        },
+                        {
+                            slug: "2019",
+                            displayName: '2019'
+                        },
+                        {
+                            slug: "2018",
+                            displayName: '2018'
+                        },
+                        {
+                            slug: "2017",
+                            displayName: '2017'
+                        },
+                        {
+                            slug: "2016",
+                            displayName: '2016'
+                        },
+                        {
+                            slug: "2015",
+                            displayName: '2015'
+                        },
+                        {
+                            slug: "2014",
+                            displayName: '2014'
+                        },
+                        {
+                            slug: "2013",
+                            displayName: '2013'
+                        },
+                        {
+                            slug: "2012",
+                            displayName: '2012'
+                        },
+
+                    ]}
+                    menuPosition="bottom"
+                    multiSelect
                 />
-                <Divider />
+                {/* <Divider />
                 <CustomSlider
                     label="Mileage"
                     labelRight={"+ mi"}
@@ -296,21 +270,13 @@ const AddListing = function ({ onClose, selectedValue, open }) {
                     menuPosition="bottom"
                     multiSelect
                 />
-                {/* <CustomSlider
-                    label="Seats"
-                    valueMatchLabel="Any"
-                    labelRight="seats"
-                    // labelLeft
-                    // labelSlideLeft
-                    // labelSlideRight
-                    min={INITIAL_CAR_STATE.seats[0]}
-                    max={INITIAL_CAR_STATE.seats[1]}
+                <CustomItem
                     name="seats"
-                    value={seats}
-                    // onChange={handleChange}
-                    onChange={onSeatsChange}
-                /> */}
-
+                    label="Seats"
+                    onClick={(e, value, name) => onSelected(e, value, name)}
+                    data={INITIAL_CUSTOM_DATA.seats}
+                    menuPosition="bottom"
+                />
 
 
                 <FormControl fullWidth >
