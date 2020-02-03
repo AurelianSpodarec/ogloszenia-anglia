5f054f06e0d5f17316fe156d91c6ec0ff05c9dc7
Aurelian <aurelians@libero.it>
2020-02-03 04:43:46 +0000

    Updated Readme

--- src/views/Categories/Cars/sub-components/CarItem/index.js
+++ src/views/Categories/Cars/sub-components/CarItem/index.js
@@ -16,10 +16,77 @@ import Skeleton from '@material-ui/lab/Skeleton';
 
 import useStyles from './styles';
 
+const CreatedSince = function ({ createdAt }) {
+
+    // If below 1h: Added 45 minutes ago
+    // Each hour up to 24h: Added 5hours ago
+    // Each day up to 30days: Added 28days ago
+    // Each month up to 12months: Added 4months ago
+    // Each year: Added 1years ago
+
+    // If its 3days or ealier, add color red and fire
+    // if its 3days ore more, add color grey and a clock
+
+    let time = '';
+
+    let getTodayDate = new Date();
+    let getCreatedDate = new Date(createdAt);
+
+    // const todayDate = {
+    //     year: getTodayDate.getFullYear(),
+    //     month: getTodayDate.getMonth(),
+    //     day: getTodayDate.getDay(),
+    //     hour: getTodayDate.getHours(),
+    //     min: getTodayDate.getMinutes(),
+    //     sec: getTodayDate.getSeconds()
+    // }
+
+    // const carDate = {
+    //     year: getCreatedDate.getFullYear(),
+    //     month: getCreatedDate.getMonth(),
+    //     day: getCreatedDate.getDay(),
+    //     hour: getCreatedDate.getHours(),
+    //     min: getCreatedDate.getMinutes(),
+    //     sec: getCreatedDate.getSeconds()
+    // }
+
+    // const diff = {
+    //     year: todayDate.year - carDate.year,
+    //     month: todayDate.month - carDate.month,
+    //     day: todayDate.day - carDate.day,
+    //     hour: todayDate.hour - carDate.hour,
+    //     min: todayDate.min - carDate.min,
+    //     sec: todayDate.sec - carDate.sec,
+    // }
+
+    var diff = (getTodayDate.getTime() - getCreatedDate.getTime()) / 1000;
+    diff /= 60;
+
+    // if(/)
+
+    // if (diff.getMinutes() >= 60) {
+    //     time = diff + 'minutes ago'
+    // } else if (diff.getHours() >= 24) {
+    //     time = diff + 'hours ago'
+    // }
+
+    // const numberDiff = new Date(Math.abs(Math.round(diff)))
+
+    console.log("Today date", new Date(Math.abs(Math.round(diff))))
+    // console.log("TODAY DATE", diff.year, diff.month, diff.day, diff.hour, diff.min, diff.sec)
+
+
+    return (
+        <Box>
+            {time}
+        </Box>
+    )
+}
+
 const CarItem = function (props) {
     const classes = useStyles();
     let { path } = useRouteMatch();
-    console.log("ite", props.car)
+    // console.log("ite", props.car)
     return (
         <Grid item xs={12} sm={6} md={12}>
             <Link className={classes.link} to={props.isLoading ? "" : `${path}/${props.car._id}`}> :
@@ -49,6 +116,12 @@ const CarItem = function (props) {
                                 <>
                                     <Box justifyContent="space-between" className={classes.detail}>
                                         <Typography className={classes.title}>{props.car.title}</Typography>
+
+
+                                        {/* START TEST */}
+                                        {/* <CreatedSince createdAt={props.car.createdAt} /> */}
+                                        {/* END TEST */}
+
                                         <Typography className={classes.price}>{new Intl.NumberFormat('en-US', {
                                             style: 'currency',
                                             currency: 'GBP',
